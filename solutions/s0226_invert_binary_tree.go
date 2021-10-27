4f1a8845c7e46879c2e8c369603cd26c8167eb61
egregors <root@egregors.com>
2021-10-27 19:11:55 +0300

    s0226 solved

---
+++ solutions/s0226_invert_binary_tree.go
@@ -0,0 +1,27 @@
+/*
+	https://leetcode.com/problems/invert-binary-tree/submissions/
+
+	Given the root of a binary tree, invert the tree, and return its root.
+*/
+
+package solutions
+
+// TreeNode just a node
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
---
+++ solutions/s0226_invert_binary_tree_test.go
@@ -0,0 +1,73 @@
+package solutions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_invertTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{"empty", args{root: nil}, nil},
+		{
+			"case 1",
+			args{
+				root: &TreeNode{
+					Val:   2,
+					Left:  &TreeNode{Val: 1},
+					Right: &TreeNode{Val: 3},
+				},
+			},
+			&TreeNode{
+				Val:   2,
+				Left:  &TreeNode{Val: 3},
+				Right: &TreeNode{Val: 1},
+			},
+		},
+		{
+			"case 2",
+			args{
+				root: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val:   2,
+						Left:  &TreeNode{Val: 1},
+						Right: &TreeNode{Val: 3},
+					},
+					Right: &TreeNode{
+						Val:   7,
+						Left:  &TreeNode{Val: 6},
+						Right: &TreeNode{Val: 9},
+					},
+				},
+			},
+			&TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val:   7,
+					Left:  &TreeNode{Val: 9},
+					Right: &TreeNode{Val: 6},
+				},
+				Right: &TreeNode{
+					Val:   2,
+					Left:  &TreeNode{Val: 3},
+					Right: &TreeNode{Val: 1},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := invertTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("invertTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
