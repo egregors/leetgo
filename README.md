f5f457b8ca38b54856a517cff8c6a9dacc61b672
egregors <root@egregors.com>
2021-10-27 19:19:41 +0300

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetgo
 LeetCode in Go, just for fun obviously 
+
+[s0226 Invert Binary Tree](/solutions/s0226_invert_binary_tree.go)
--- solutions/s0226_invert_binary_tree.go
+++ solutions/s0226_invert_binary_tree.go
@@ -1,5 +1,5 @@
 /*
-	https://leetcode.com/problems/invert-binary-tree/submissions/
+	https://leetcode.com/problems/invert-binary-tree/
 
 	Given the root of a binary tree, invert the tree, and return its root.
 */
