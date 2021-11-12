5c5681bde83c997c1d94ced0f7bd67330e656546
egregors <root@egregors.com>
2021-11-12 11:10:25 +0200

    s0226 refactoring

--- solutions/s0226_invert_binary_tree.go
+++ solutions/s0226_invert_binary_tree.go
@@ -17,11 +17,6 @@ func invertTree(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-
-	root.Left, root.Right = root.Right, root.Left
-
-	invertTree(root.Left)
-	invertTree(root.Right)
-
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
 	return root
 }
