9d772efef22733a18666453892c3db71fc365c27
egregors <root@egregors.com>
2021-11-24 18:40:25 +0300

    s0026 solved

---
+++ solutions/s0026_remove_duplicates_from_sorted_array_test.go
@@ -0,0 +1,36 @@
+package solutions
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				nums: []int{1, 1, 2},
+			},
+			want: 2,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				nums: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
