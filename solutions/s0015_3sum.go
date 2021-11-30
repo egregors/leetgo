d4bbaf7bda8300d238ee50adf29c70e412a21bbd
egregors <root@egregors.com>
2021-11-30 14:10:52 +0300

    s0015 solved - two pointers

---
+++ solutions/s0015_3sum.go
@@ -0,0 +1,47 @@
+/*
+	https://leetcode.com/problems/3sum/
+
+	Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+	such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+	Notice that the solution set must not contain duplicate triplets.
+*/
+
+package solutions
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	res := &[][]int{}
+	sort.Ints(nums)
+	for i, n := range nums {
+		if n > 0 {
+			break
+		}
+		if i == 0 || nums[i-1] != nums[i] {
+			twoSumII(nums, i, res)
+		}
+	}
+	return *res
+}
+
+func twoSumII(nums []int, i int, res *[][]int) {
+	lo, hi := i+1, len(nums)-1
+
+	for lo < hi {
+		sum := nums[i] + nums[lo] + nums[hi]
+		if sum < 0 {
+			lo++
+		} else if sum > 0 {
+			hi--
+		} else {
+			*res = append(*res, []int{nums[i], nums[lo], nums[hi]})
+			lo++
+			hi--
+			for lo < hi && nums[lo] == nums[lo-1] {
+				lo++
+			}
+		}
+
+	}
+}
---
+++ solutions/s0015_3sum_test.go
@@ -0,0 +1,54 @@
+package solutions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				nums: []int{-1, 0, 1, 2, -1, -4},
+			},
+			want: [][]int{
+				{-1, -1, 2},
+				{-1, 0, 1},
+			},
+		},
+		{
+			name: "Test 2",
+			args: args{
+				nums: []int{0, 0, 0},
+			},
+			want: [][]int{
+				{0, 0, 0},
+			},
+		},
+		{
+			name: "Test 3",
+			args: args{
+				nums: []int{-1, 0, 1, 2, -1, -4},
+			},
+			want: [][]int{
+				{-1, -1, 2},
+				{-1, 0, 1},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
