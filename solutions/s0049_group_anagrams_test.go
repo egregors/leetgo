5a0368b6adc664c27a280bbbf11a89b0a2927c62
egregors <root@egregors.com>
2022-01-04 18:33:06 +0300

    fix test

--- solutions/s0049_group_anagrams_test.go
+++ solutions/s0049_group_anagrams_test.go
@@ -1,45 +1,27 @@
 package solutions
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_groupAnagrams(t *testing.T) {
-	type args struct {
-		strs []string
-	}
-	tests := []struct {
-		name string
-		args args
-		want [][]string
-	}{
-		{
-			name: "Test 1",
-			args: args{
-				strs: []string{"eat", "tea", "tan", "ate", "nat", "bat"},
-			},
-			want: [][]string{
-				{"eat", "tea", "ate"},
-				{"tan", "nat"},
-				{"bat"},
-			},
-		},
-		{
-			name: "Test 2",
-			args: args{
-				strs: []string{"", ""},
-			},
-			want: [][]string{
-				{"", ""},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := groupAnagrams(tt.args.strs); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("groupAnagrams() = %v, want %v", got, tt.want)
-			}
-		})
+	// t1
+	assert.Equal(t, [][]string{
+		{"", ""},
+	}, groupAnagrams([]string{"", ""}))
+
+	// t2
+	r := groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
+	for _, el := range r {
+		switch len(el) {
+		case 1:
+			assert.Equal(t, el, []string{"bat"})
+		case 2:
+			assert.Equal(t, el, []string{"tan", "nat"})
+		case 3:
+			assert.Equal(t, el, []string{"eat", "tea", "ate"})
+		}
 	}
 }
