0d179411fe059f562fec4b37e3398081fc27b04a
egregors <root@egregors.com>
2022-01-13 13:53:44 +0300

    s0238 solved - two pointers, prefix production

---
+++ solutions/s0238_product_of_array_except_self.go
@@ -0,0 +1,31 @@
+/*
+	https://leetcode.com/problems/product-of-array-except-self/
+
+	Given an integer array nums, return an array answer such that answer[i] is equal to the
+	product of all the elements of nums except nums[i].
+
+	The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+	You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
+
+package solutions
+
+func productExceptSelf(nums []int) []int {
+	l := make([]int, len(nums))
+	r := make([]int, len(nums))
+	l[0] = 1
+	r[len(r)-1] = 1
+	res := make([]int, len(nums))
+
+	for i := 1; i < len(nums); i++ {
+		l[i] = l[i-1] * nums[i-1]
+		r[len(r)-1-i] = nums[len(nums)-i] * r[len(r)-i]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		res[i] = l[i] * r[i]
+	}
+
+	return res
+}
---
+++ solutions/s0238_product_of_array_except_self_test.go
@@ -0,0 +1,31 @@
+package solutions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_productExceptSelf(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Example 1",
+			args: args{
+				nums: []int{1, 2, 3, 4},
+			},
+			want: []int{24, 12, 8, 6},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, productExceptSelf(tt.args.nums), "productExceptSelf(%v)", tt.args.nums)
+		})
+	}
+}
