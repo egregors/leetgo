52cd30b48b068fbbfe4729860c364777e9953fd6
egregors <root@egregors.com>
2022-01-22 16:43:03 +0300

    add minimum and remove from int slice helpers

--- solutions/utils.go
+++ solutions/utils.go
@@ -9,3 +9,20 @@ func LinkedListLen(n *ListNode) int {
 	}
 	return c
 }
+
+// Minimum return minimum element of Int slice
+func Minimum(xs ...int) int {
+	min := 1<<31 - 1
+	for _, x := range xs {
+		if x < min {
+			min = x
+		}
+	}
+	return min
+}
+
+// RemoveFromIntSlice removes k-th element of Int slice in-place
+// todo: wanna move to go2 with generics
+func RemoveFromIntSlice(xs *[]int, k int) {
+	*xs = append((*xs)[:k], (*xs)[k+1:]...)
+}
