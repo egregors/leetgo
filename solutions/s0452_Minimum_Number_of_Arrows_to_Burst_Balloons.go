5a2e97777a59392a39b19488c8c359b8606f041a
egregors <egregors@pm.me>
2023-01-06 10:16:03 +0100

    s0452 solved - greedy [fix]

--- solutions/s0452_Minimum_Number_of_Arrows_to_Burst_Balloons.go
+++ solutions/s0452_Minimum_Number_of_Arrows_to_Burst_Balloons.go
@@ -20,7 +20,7 @@ import "sort"
 
 func findMinArrowShots(points [][]int) int {
 	sort.Slice(points, func(i, j int) bool {
-		return points[i][0] < points[j][0]
+		return points[i][1] < points[j][1]
 	})
 
 	res := 1
