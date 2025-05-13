17101234d29b6bb172a10f8da46e1612692b1bb8
Vadim Iskuchekov <egregors@pm.me>
2025-05-13 19:37:02 +0000

    s1848 solved – simulation

---
+++ solutions/s1848_minimum_distance_to_the_target_element.go
@@ -0,0 +1,34 @@
+/*
+	https://leetcode.com/problems/minimum-distance-to-the-target-element
+
+	Given an integer array nums (0-indexed) and two integers target and start, find an index i such that
+	nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+
+	Return abs(i - start).
+
+	It is guaranteed that target exists in nums.
+*/
+
+package solutions
+
+import "math"
+
+func getMinDistance(nums []int, target int, start int) int {
+	val := math.MaxInt
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target {
+			val = min(val, abs(i-start))
+		}
+	}
+
+	return val
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+
+	return x
+}
