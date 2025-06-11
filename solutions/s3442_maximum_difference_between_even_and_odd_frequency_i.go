47197263f1d03abfd3947d79f19e5d23693d9b62
egregors <egregors@pm.me>
2025-06-11 13:51:19 +0200

    s3442 solved - arrays

--- solutions/s3442_maximum_difference_between_even_and_odd_frequency_i.go
+++ solutions/s3442_maximum_difference_between_even_and_odd_frequency_i.go
@@ -3,7 +3,8 @@
 
 	You are given a string s consisting of lowercase English letters.
 
-	Your task is to find the maximum difference diff = freq(a1) - freq(a2) between the frequency of characters a1 and a2 in the string such that:
+	Your task is to find the maximum difference diff = freq(a1) - freq(a2) between
+		the frequency of characters a1 and a2 in the string such that:
 
 		a1 has an odd frequency in the string.
 		a2 has an even frequency in the string.
